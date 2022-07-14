cdb6805c4593b73bb2a78163c3764fd285803820
Dangana Rayam Inebhari <96420808+RayamQuan@users.noreply.github.com>
2022-07-14 17:00:54 +0000

    final version of assignment

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum =0;
+
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum+=arrayOfNumbers[i]
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,20 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let length=arrayOfNumbers.length;
+  let i=0;
+  let evenNumbers=0;
+
+  while (i<length) {
+    if (arrayOfNumbers[i] %2==0) 
+      {
+        evenNumbers +=1;
+      }
+      i++;
+    }
+    return (evenNumbers);
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
